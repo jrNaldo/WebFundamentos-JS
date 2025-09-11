cacb7f4bd1eb56e3ea70034535ce458b65f4ca1d
Naldo <lucirleylucas987@gmail.com>
2025-09-11 10:49:58 -0300

    Exercicio de Eventos

--- Eventos/js/formulario.js
+++ Eventos/js/formulario.js
@@ -1,66 +1,72 @@
-(function(){
+
+(function () {
     'use strict';
 
     const txtTitulo = document.getElementById("txtTitulo")
     const btn = document.getElementById("btn")
     const formCadastro = document.querySelector(".formCadastro")
-    const txtDescricao = document.getElementById("txtDescricao")
-    const contadorCont = document.getElementById("contador")
-    const resta = contadorCont.getElementsByTagName("span")[0]
-    const max = parseInt(txtDescricao.maxLength)
-    const chk = document.getElementById("chkAceito")
+
+    formCadastro.addEventListener("submit", function (e) {
+        console.log(txtTitulo.value)
+        if (!txtTitulo.value) {
+            showErrorMessage("Preencha todos os campos", function () {
+                txtTitulo.focus()
+            })
+            e.preventDefault()
+
+        }
+    })
+
     const feedbackMessage = document.getElementById("feedbackMessage")
     const feedbackMessageCloseBtn = feedbackMessage.getElementsByTagName("button")[0]
 
-    function mostrarNumero(n){
-        resta.textContent = n
-    }
-
-    mostrarNumero(max)
-
-    
-    formCadastro.addEventListener("submit", function(e){
-        if(!txtTitulo.value){
-            showErrorMsg("Preencha esse campo", function(){
-                txtTitulo.focus()
-            })
-            e.preventDefault() // Previne o comportamento padrão do browser
-        }
-    })
-    
-    function showErrorMsg(msg, cb){
+    function showErrorMessage(msg, cb) {
         feedbackMessage.classList.add("show")
         feedbackMessage.getElementsByTagName("p")[0].textContent = msg
 
-        function hideErrorMessage(){
-            console.log("chamada...")
+
+        function hideErrorMessage() {
+            console.log("clicado close")
             feedbackMessage.classList.remove("show")
             feedbackMessageCloseBtn.removeEventListener("click", hideErrorMessage)
         }
-
         feedbackMessageCloseBtn.addEventListener("click", hideErrorMessage)
-        if(typeof cb === "function"){
+
+        if (typeof cb === "function") {
             cb()
         }
     }
-    
-    contadorCont.removeAttribute("style")
-    // contadorCont.style.display = "block"
 
-    // txtDescricao.addEventListener("input", function(){
-    //     let numLetrasDigitados = this.value.length
-    //     let caracteresRestantes = max - parseInt(numLetrasDigitados)
-    //     resta.textContent = caracteresRestantes
-    // })
-    
 
-    txtDescricao.addEventListener("input", ()=>{
-        mostrarNumero(max - parseInt(txtDescricao.value.length))
-    })
+
+
+    const txtDescricao = document.getElementById("txtDescricao")
+    const contadorContainer = document.getElementById("contador")
+    const resta = contadorContainer.getElementsByTagName("span")[0]
+    const maxima = txtDescricao.maxLength
+
+    mostrarNumero(maxima)
+
+    // contadorContainer.removeAttribute("style")
+    contadorContainer.style.display = "block"
+
+    function checkLength() {
+        let numeroLetrasDigitdas = this.value.length
+        let caractersRestantes = parseInt(maxima) - parseInt(numeroLetrasDigitdas)
+        mostrarNumero(caractersRestantes)
+    }
+
+    function mostrarNumero(n) {
+        resta.textContent = n
+    }
+    txtDescricao.addEventListener("input", checkLength)
 
     btn.disabled = true
 
-    chk.addEventListener("change", function(){
+    const chkAceito = document.getElementById("chkAceito")
+    chkAceito.addEventListener("change", function () {
         btn.disabled = !this.checked
     })
-})();
+
+
+})()
