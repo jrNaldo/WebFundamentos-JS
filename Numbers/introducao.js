9628b0d15be01d71e7bf00ce33620a3f288d85f9
Naldo <lucirleylucas987@gmail.com>
2025-09-23 21:12:31 -0300

    introdução a Numbers

---
+++ Numbers/introducao.js
@@ -0,0 +1,14 @@
+let numero = 123.4567
+
+console.log(numero.toFixed(2)) // Quantidade de casas decimais que quer que mostre
+console.log(numero.toPrecision(5)) // Num de digitos que eu quero em um numero 
+
+numero = 12.34567
+console.log(numero.toExponential())
+
+numero = 11
+console.log(numero.toString(16))
+
+numero = 12345.67
+console.log(numero.toLocaleString("pt-br"))
+console.log(numero.toLocaleString("pt-pt", {style: "currency", currency: "EUR"}))
